1c2b7fcdac06f3d2234d43097c03aac0d2bbca74
Zubair Naseem <zubair.naseem@nordsec.com>
2024-05-13 13:38:54 +0200

    feat node server

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import cors from 'cors';
 import express from 'express';
 
 const app = express();
-
-app.use(cors());
+var corsOptions = {
+  origin: 'http://example.com',
+  // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+// app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://superman.com');
+  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');
+  res.setHeader('Cross-origin-Opener-Policy','same-origin');
+  res.setHeader('Cross-origin-Resource-Policy','same-origin');
   res.send('Hello World!');
 });
 
